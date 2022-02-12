d11700e3ed49f63fc552869838b880ded1dd3cd8
flo-tech <florian.vanier@epitech.eu>
2022-02-12 16:54:58 +0100

    [+] incress gold in main and set in Gold.gd

--- scenes/main/Gold.gd
+++ scenes/main/Gold.gd
@@ -1,14 +1,9 @@
 extends Label
 
-var gold = 0
-
 func _ready():
 	pass # Replace with function body.
 
-func earn_money(amount):
-	gold += amount
-	var text = "Argent = " + String(gold) + " $"
-	set_text(text)
-
 func _process(_delta):
-	earn_money(50)
+	var main = get_parent()
+	var text = "Argent = " + String(main.gold) + " $"
+	set_text(text)
--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+var gold = 0
+onready var gold_label = $Gold
 onready var timer = $Timer
 
 func _ready():
 	pass
 
-func _physics_process(_delta):
+func earn_money(amount):
+	gold += amount
+	
+
+func _physics_process(delta):
 	if Input.get_action_strength("ui_left"):
+		earn_money(50)
 		print(timer)
 
